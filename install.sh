99bc90bb9e0e582b401fddad40548b2056f20eff
Rimjact <Rimjact@ya.ru>
2026-08-10 00:06:52 +0300

    fix(install.sh): fix installation

--- install.sh
+++ install.sh
@@ -31,21 +31,43 @@ create_directories() {
     echo "--- ✅ Директории созданы ---"
 }
 
+try_download_file() {
+    local file="$1"
+    local dest="$2"
+
+    wget -q -O "$dest" "$file" >/dev/null 2>&1 && return 0
+
+    return 1
+}
+
 download_file() {
     local file_path="$1"
     local dest="$2"
 
+    local status=0
+
     echo "🔃 Скачиваем файл: $1"
-    wget -q -O "$dest" "${REPOSITORY}/${BRANCH}/${file_path}" >/dev/null 2>&1 && return 0
+    if try_download_file "${REPOSITORY}/${BRANCH}/${file_path}" "$dest"; then
+        status=1
+    fi
+
+    if [ "$status" -ne 1 ]; then
+        echo "❌ Не удалось скачать файл ${file_path}. Попробуйте повторить установку позже." >&2
+        exit 1
+    fi
 }
 
 download_project_files() {
     echo "--- ⚙️ Скачиваем файлы проекта ---"
 
-    download_file "root/etc/init.d/wifistats" "/etc/init.d/wifistats"
-    download_file "usr/bin/wifistats" "/usr/bin/wifistats"
-    download_file "usr/lib/lua/luci/controller/wifistats.lua" "/usr/lib/lua/luci/contorller/wifistats.lua"
-    download_file "usr/lib/lua/luci/view/wifistats.htm" "/usr/lib/lua/luci/view/wifistats.htm"
+    local downloaded=1
+
+    if download_file "root/etc/init.d/wifistats" "/etc/init.d/wifistats"; then
+
+    fi
+    download_file "root/usr/bin/wifistats" "/usr/bin/wifistats"
+    download_file "root/usr/lib/lua/luci/controller/wifistats.lua" "/usr/lib/lua/luci/contorller/wifistats.lua"
+    download_file "root/usr/lib/lua/luci/view/wifistats.htm" "/usr/lib/lua/luci/view/wifistats.htm"
 
     echo "--- ✅ Файлы проекта скачаны ---"
 }
@@ -56,7 +78,7 @@ start_daemon() {
     echo "📕 Регулируем права..."
     chmod 755 /etc/init.d/wifistats
     chmod 755 /usr/bin/wifistats
-    chmod 644 /etc/wifistas
+    chmod 644 /etc/wifistats
 
     echo "👾 Выпускаем демонов..."
     /etc/init.d/wifistats enable >/dev/null 2>&1 || true
@@ -76,7 +98,7 @@ install_project() {
     start_daemon
 
     echo "=== 🎉 Wi-Fi Stats установлен ==="
-    echo "Мониторинг доступен Статус -> Статистика Wi-Fi"
+    echo "Мониторинг доступен на Статус -> Статистика Wi-Fi"
 
     rm -f /tmp/wifistats-install.sh 2>/dev/null || true
 }
